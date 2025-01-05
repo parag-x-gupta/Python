fdc02b6af750cc68e43edcdbfb34724332ac1d42
parag-x-gupta <gupta.parag@protonmail.com>
2025-01-04 22:14:50 -0500

    learning list methods
    Some of the list methods. Learning in positron

---
+++ list_methods.py
@@ -0,0 +1,17 @@
+import sys
+
+print("System version :" , sys.version)
+print("Source python file path :", sys.executable)
+
+
+my_list = [10,20,30,12,434,453,234,567,54,4323,43]
+
+print("List is : ", my_list)
+
+# .count() -> number of instancee of an element
+# .insert() -> insert element at an index
+# .sort()/.sorted() -> sorts the list
+# .pop() -> remove element from the list
+# .range() -> range object, iterable
+# .len() -> length of the iterable
+
